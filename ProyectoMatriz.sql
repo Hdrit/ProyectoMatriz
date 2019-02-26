e418f5aaf84d781de0501c4ffa44f5140a212851
Hdrit <Hdritleolo@gmail.com>
2019-02-25 21:00:07 -0500

    Fix matrix naming

--- ProyectoMatriz.sql
+++ ProyectoMatriz.sql
@@ -3,18 +3,18 @@ Especificacion del paquete que maneja el laberinto
 */
 CREATE OR REPLACE PACKAGE laberinto AS
 --Definicion del tipo matriz
-  TYPE columna IS
+  TYPE coordenate IS
     TABLE OF NUMBER(1) INDEX BY BINARY_INTEGER;
-  TYPE fila IS
-    TABLE OF columna INDEX BY BINARY_INTEGER;
+  TYPE matrix IS
+    TABLE OF coordenate INDEX BY BINARY_INTEGER;
 --Constantes de coordenadas
   y_index NUMBER := 1;
   x_index NUMBER := 2;
 --Halla el camino de a la salida. No camino if null. 
   FUNCTION hallar_camino (
-    coordenada_inicio   IN columna,
-    matriz              IN fila
-  ) RETURN fila;
+    coordenada_inicio   IN coordenate,
+    matriz              IN matrix
+  ) RETURN matrix;
 
 /* --private
 Revisa la matriz
@@ -49,9 +49,9 @@ CREATE OR REPLACE PACKAGE BODY laberinto AS
   --Funcion privada norte, sur, este y oeste
 
   FUNCTION norte (
-    coordenada IN columna
-  ) RETURN columna AS
-    nueva_coordenada   columna;
+    coordenada IN coordenate
+  ) RETURN coordenate AS
+    nueva_coordenada   coordenate;
   BEGIN
     nueva_coordenada(y_index) := coordenada(y_index) - 1;
     nueva_coordenada(x_index) := coordenada(x_index);
@@ -59,9 +59,9 @@ CREATE OR REPLACE PACKAGE BODY laberinto AS
   END norte;
 
   FUNCTION sur (
-    coordenada IN columna
-  ) RETURN columna AS
-    nueva_coordenada   columna;
+    coordenada IN coordenate
+  ) RETURN coordenate AS
+    nueva_coordenada   coordenate;
   BEGIN
     nueva_coordenada(y_index) := coordenada(y_index) + 1;
     nueva_coordenada(x_index) := coordenada(x_index);
@@ -69,9 +69,9 @@ CREATE OR REPLACE PACKAGE BODY laberinto AS
   END sur;
 
   FUNCTION este (
-    coordenada IN columna
-  ) RETURN columna AS
-    nueva_coordenada   columna;
+    coordenada IN coordenate
+  ) RETURN coordenate AS
+    nueva_coordenada   coordenate;
   BEGIN
     nueva_coordenada(y_index) := coordenada(y_index);
     nueva_coordenada(x_index) := coordenada(x_index + 1);
@@ -79,9 +79,9 @@ CREATE OR REPLACE PACKAGE BODY laberinto AS
   END este;
 
   FUNCTION oeste (
-    coordenada IN columna
-  ) RETURN columna AS
-    nueva_coordenada   columna;
+    coordenada IN coordenate
+  ) RETURN coordenate AS
+    nueva_coordenada   coordenate;
   BEGIN
     nueva_coordenada(y_index) := coordenada(y_index);
     nueva_coordenada(x_index) := coordenada(x_index) - 1;
@@ -89,10 +89,10 @@ CREATE OR REPLACE PACKAGE BODY laberinto AS
   END oeste;
 
   FUNCTION camino_recursivo (
-    coordenada   IN columna,
-    matriz       IN fila
-  ) RETURN fila AS
-    camino   fila;
+    coordenada   IN coordenate,
+    matriz       IN matrix
+  ) RETURN matrix AS
+    camino   matrix;
   BEGIN
   --1.	if (x,y fuera del laberinto) return false 
     IF ( coordenada(y_index) <= 0 OR coordenada(x_index) <= 0 OR coordenada(y_index) > matriz.count OR coordenada(x_index) > matriz
@@ -143,9 +143,9 @@ CREATE OR REPLACE PACKAGE BODY laberinto AS
   --declaracion de la funcion Hallar_camino
 
   FUNCTION hallar_camino (
-    coordenada_inicio   IN columna,
-    matriz              IN fila
-  ) RETURN fila AS
+    coordenada_inicio   IN coordenate,
+    matriz              IN matrix
+  ) RETURN matrix AS
   BEGIN
   --Retorna el camino en reversa
     RETURN camino_recursivo(coordenada_inicio,matriz);
